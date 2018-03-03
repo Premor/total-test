a03b166108ce75242a0ddece76cc5f91acf4f84a
Premor <spamerkik@gmail.com>
2018-03-03 18:26:08 +0300

    auth work

---
+++ controllers/default.js
@@ -0,0 +1,55 @@
+exports.install = function() {
+	ROUTE('/');
+	
+	ROUTE('/registration',view_registration,['unauthorize']);
+	ROUTE('/registration',view_registration_auth,['authorize']);
+	ROUTE('/registration', json_create_user, ['post','unauthorize']);
+	ROUTE('/login');
+	ROUTE('/login', login, ['post']);
+	ROUTE('/logout',logout,['authorize'])
+	//ROUTE('/congratulation')
+	// or
+	// F.route('/');
+};
+
+function logout(){
+	this.cookie('user','','-1 day')
+	this.redirect('/')
+}
+
+function view_index() {
+	var self = this;
+	self.view('index');
+}
+
+function view_registration_auth(){
+	var self = this;
+	self.view('registration_auth');
+}
+
+function view_registration(){
+	var self = this;
+	self.view('registration');
+}
+
+function login(){
+	MODEL('user').find_u(this.body.login, this.body.psw, (err,res)=>{
+		if (err)
+			this.view('fail', err);
+		else
+		{
+			this.cookie('user', res.id, '5 days');
+			this.redirect('/');
+		}
+	})
+}
+function json_create_user() {
+	var user = { id: Utils.GUID(5), login: this.body.login, password: this.body.psw };
+	
+
+	//console.log(this.flags)
+	//console.log(this.body)
+    // global alias:
+    MODEL('user').create(user);
+    this.view('/congratulation', user);
+}
---
+++ definitions/authorize.js
@@ -0,0 +1,31 @@
+framework.onAuthorize = function(req, res, flags, next) {
+
+    // EXAMPLE:
+    var userId = req.cookie('user');
+
+    if (userId === undefined) {
+        // unauthorized user
+        next(false);
+        return;
+    }
+
+    var nosql = NOSQL('users');
+
+    nosql.one().where('id', userId).callback(function(err, user) {
+
+        if (err || !user) {
+            // unauthorized user
+            next(false);
+            return;
+        }
+
+        // user is authorized
+        next(true, user);
+
+        // or assign the user object to request.user:
+        /*
+            req.user = user;
+            next(true);
+        */
+    });
+}
